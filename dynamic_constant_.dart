806d1d10e2bc9535cbc1d98a753eaf19d1ec59d5
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-06-11 07:55:13 +0530

    dynamic and constant

---
+++ dynamic_constant_.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+  dynamic value = null;
+  print(value);
+
+  value = "India";
+  print(value);
+
+  value = 91;
+  print(value);
+
+  value = 3.14;
+  print(value);
+
+  value = true;
+  print(value);
+
+  //final or constant variables
+  final name = "THE EASYLEARN ACADEMY";
+  print(name);
+
+  final String address = "Hill Drive, Bhavnagar";
+  print(address);
+
+  const pi = 3.14; //inferred typing
+  print(pi);
+
+  const double somevalue = 12.7;
+  print(somevalue);
+  // pi = 3.1417;
+  print(pi);
+}
