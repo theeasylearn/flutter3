a4c6c8665c4c11cca30b94cbbe4d8ffa50efd659
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-06-25 10:04:42 +0530

    if else dart

---
+++ ifelse1.dart
@@ -0,0 +1,16 @@
+//write a program to findout whether shape(length,width) is square or not using given input
+import 'dart:io';
+
+void main() {
+  int length, width;
+  print("Enter shape's length");
+  length = int.parse(stdin.readLineSync().toString());
+  print("Enter shape's width");
+  width = int.parse(stdin.readLineSync().toString());
+  if (length == width) // < > <= >= == !=
+  {
+    print("shape is square");
+  } else {
+    print("shape is not square");
+  }
+}
