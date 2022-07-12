697fc3ff872ce06817762fa02c77d27a0952bcc2
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-07-12 11:05:34 +0530

    map

--- map1.dart
+++ map1.dart
@@ -1,29 +1,32 @@
 import 'dart:collection';
 
 void main() {
-  //class object = new class()
-  HashMap map1 = new HashMap();
-  //class object = new class()
-  LinkedHashMap map2 = new LinkedHashMap();
-  //class object = new class()
-  SplayTreeMap map3 = new SplayTreeMap();
-
-  map1['name'] = ["ankit patel"];
-  map1['age'] = 37;
-  map1['weight'] = 80.25;
+  //create map type object
+  HashMap map1 = new HashMap<String, dynamic>();
+  map1['name'] = "Ankit Patel";
+  map1['country'] = "USA";
+  map1['age'] = 38;
+  map1['weight'] = 75.25;
   map1['gender'] = true;
   print(map1);
-  map2['name'] = "ankit patel";
-  map2['age'] = 37;
-  map2['weight'] = 80.25;
+
+  LinkedHashMap map2 = new LinkedHashMap<String, dynamic>();
+  map2['name'] = "Ankit Patel";
+  map2['country'] = "USA";
+  map2['age'] = 38;
+  map2['weight'] = 75.25;
   map2['gender'] = true;
   print(map2);
-  map3['name'] = "ankit patel";
-  map3['age'] = 37;
-  map3['weight'] = 80.25;
+
+  SplayTreeMap map3 = new SplayTreeMap<String, dynamic>();
+  map3['name'] = "Ankit Patel";
+  map3['country'] = "USA";
+  map3['age'] = 38;
+  map3['weight'] = 75.25;
   map3['gender'] = true;
   print(map3);
 
-  print("Name is " + map1['name']);
-  print("age is " + map1['age'].toString());
+  print('name in map1 ' + map1['name']);
+  print('name in map2 ' + map2['name']);
+  print('name in map3 ' + map3['name']);
 }
--- map2.dart
+++ map2.dart
@@ -1,27 +1,33 @@
 import 'dart:collection';
 
 void main() {
-  //dynamic type map where key is string and value is dynamic
-  Map course = {
-    'name': 'Flutter',
-    'duration': 120,
-    'fees': 12000.99,
-    'teacher': 'Ankit',
-    'isCertified': true,
-    'secret': null
+  Map<String, dynamic> course1 = {
+    "name": "flutter",
+    "duration": 150,
+    "fees": 12000.99,
+    "isCertified": true
   };
+  print(course1);
+  Map course2 = {
+    "name": "flutter",
+    "duration": 150,
+    "fees": 12000.99,
+    "isCertified": true
+  };
+  print(course2);
 
-  print(course);
-  print(course.isEmpty); //false
-  print(course.isNotEmpty); //true
-  print(course.length); //true
-  print("is teacher key exists " + course.containsKey("teacher").toString());
-  print(
-      "is Flutter value exists " + course.containsValue("Flutter").toString());
-  print(course.keys);
-  print(course.values);
+  var course3 = {
+    "name": "flutter",
+    "duration": 150,
+    "fees": 12000.99,
+    "isCertified": true
+  };
+  print(course3);
 
-  course.remove("secret");
-  print(course);
-  
+  //read only map
+  Map country =
+      Map.unmodifiable({"name": "India", "code": 91, "capital": "delhi"});
+  country['name'] = "Bharat";
+  print(country);
+
 }
