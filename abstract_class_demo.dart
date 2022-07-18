3a76d1624022d51bce1363161adc5269fb652874
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-07-18 09:56:00 +0530

    abstract & hiearchical

---
+++ abstract_class_demo.dart
@@ -0,0 +1,65 @@
+// example of abstract class and hiearchical inheritance
+import 'dart:io';
+abstract class Shape {
+  double pi = 3.14;
+  double getArea(); //abtract method , it has no code
+  void display(); //abstract method, it has no code
+}
+
+class Circle extends Shape {
+  double radius = 0.0;
+  Circle(double radius) {
+    this.radius = radius;
+  }
+  double getArea() {
+    //local variable
+    double area = super.pi * this.radius * this.radius;
+    return area;
+  }
+
+  void display() {
+    print("radius = $radius");
+  }
+}
+
+class Cylinder extends Shape {
+  double radius = 0, height = 0;
+  Cylinder(double radius, double height) {
+    this.radius = radius;
+    this.height = height;
+  }
+  double getArea() {
+    //local variable
+    double area = (2 * super.pi * this.radius * this.height) +
+        (2 * super.pi * this.radius * this.radius);
+    return area;
+  }
+
+  void display() {
+    print("radius = $radius");
+    print("height = $height");
+  }
+}
+
+void main() {
+  //let us try to create object of abstract class Shape
+  // Shape s1 = new Shape();
+  print("Enter radius to find circle area");
+  double radius = double.parse(stdin.readLineSync().toString());
+  Circle c1 = new Circle(radius);
+  double area = c1.getArea();
+  print("area of circle = $area");
+  c1.display();
+
+  print("Enter radius to find cylinder area");
+  radius = double.parse(stdin.readLineSync().toString());
+  print("Enter height to find cylinder area");
+  double height = double.parse(stdin.readLineSync().toString());
+
+  Cylinder c2 = new Cylinder(radius, height);
+  area = c2.getArea();
+  print("area of Cylinder = $area");
+  c2.display();
+
+  print("Program is over");
+}
---
+++ hiearchical_inheritance_demo.dart
@@ -0,0 +1,52 @@
+//conceptual example of single level inheritance
+class Person {
+  void walk() {
+    print("I can walk");
+  }
+
+  void talk() {
+    print("I can talk");
+  }
+}
+
+class Student extends Person {
+  void read() {
+    print("I can read");
+  }
+
+  void write() {
+    print("I can write");
+  }
+
+  void WhatICanDo() {
+    super.walk();
+    super.talk();
+    this.read();
+    this.write();
+  }
+}
+
+class Teacher extends Person {
+  void teach() {
+    print("I can teach");
+  }
+
+  void help() {
+    print("I can help");
+  }
+
+  void WhatICanDo() {
+    super.walk();
+    super.talk();
+    this.teach();
+    this.help();
+  }
+}
+
+void main() {
+  Student s1 = new Student();
+  s1.WhatICanDo();
+
+  Teacher t1 = new Teacher();
+  t1.WhatICanDo();
+}
