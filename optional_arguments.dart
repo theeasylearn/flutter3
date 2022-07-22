b276abfe35d4553730b53653917d81672b5cbf83
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-07-22 11:39:24 +0530

    o

--- optional_arguments.dart
+++ optional_arguments.dart
@@ -1,22 +1,15 @@
 import 'dart:io';
 
-// concept of optional arguments
-//optional positional arguments
-int GetArea(int length, [int width = 0]) {
-  print("length = $length width = $width");
-  int area = 0;
-  if (width == 0) {
-    area = length * length;
-  } else {
-    area = length * width;
-  }
-  return area;
+// concept of optional positional arguments
+int getArea(int width, [int length = 0]) {
+  if (length != 0)
+    return width * length;
+  else
+    return width * width;
 }
 
-int GetVolume(int length, {int width = 1, int height = 1}) {
-  print("length = $length width = $width height = $height");
-  int volume = length * width * height;
-  return volume;
+int getVolume(int height, {int width = 1, int length = 1}) {
+  return height * width * length;
 }
 
 void main() {
@@ -29,11 +22,11 @@ void main() {
   print("Enter height");
   int h = int.parse(stdin.readLineSync().toString());
 
-  print("Area = " + GetArea(l).toString());
-  print("Area = " + GetArea(l, w).toString());
+  // print("Area = " + getArea(w).toString());
+  // print("Area = " + getArea(w, l).toString());
 
-  print("volume = " + GetVolume(l).toString());
-  print("volume = " + GetVolume(l,height: h).toString());
-  print("volume = " + GetVolume(l,width: w).toString());
-  print("volume = " + GetVolume(l,width: w,height: h).toString());
+  print("1 argument Volume = " + getVolume(h).toString());
+  print("2 argument Volume (with length) = " + getVolume(h,length: l).toString());
+  print("2 argument Volume (with width) = " + getVolume(h,width:w).toString());
+  print("2 argument Volume  = " + getVolume(h,width:w,length: l).toString());
 }
